ad0177747cbc04e4816bebf5f61d5c58ef603c9a
panthersClass <atiqullah_esmat@yahoo.com>
2022-11-10 11:43:23 -0800

    second commit for remote

--- src/gitpractice/FirstClassGit.java
+++ src/gitpractice/FirstClassGit.java
@@ -5,6 +5,14 @@ public class FirstClassGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		
+		
+		
+		public void test() {
+			System.out.println("this is from local repository");
+		}
+			
+		}
+	
 
-}
+
